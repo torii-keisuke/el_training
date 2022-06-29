0bfd76c32f6c939feacc7b7b9ed8a83a37a00b44
torii-keisuke <keisuke.2001.12@gmail.com>
2022-06-29 13:25:29 +0900

    3つ修正しました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-  def after_sign_out_path_for(resources)
+  def after_sign_out_path_for(_resources)
     root_path
   end
 
-  def after_sign_up_path_for(resources)
+  def after_sign_up_path_for(_resources)
     new_user_session_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :tasks
-  has_many :labels
+  has_many :tasks, dependent: :destroy
+  has_many :labels, dependent: :destroy
   validates :name, presence: true, length: { maximum: 20 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }
 end
